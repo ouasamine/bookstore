8f1e64f80d09352864adefceca41cbc044f37b33
ouasamine <ouas.amine@outlook.com>
2022-11-09 09:53:01 +0100

    Display 'Under construction' message after button click in categories.js

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,19 +1,26 @@
-import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { checkStatus } from '../redux/categories/categories';
 
 function Categories() {
+  const [msgPlaceholder, setMsg] = useState('');
   const dispatch = useDispatch();
+  dispatch(checkStatus());
+  const msg = useSelector((state) => state.handleStatus);
   const handleSubmit = (e) => {
     e.preventDefault();
-    dispatch(checkStatus());
+    setMsg(msg);
   };
   return (
-    <button
-      onClick={handleSubmit}
-      type="submit"
-    >
-      Check Status
-    </button>
+    <>
+      <button
+        onClick={handleSubmit}
+        type="submit"
+      >
+        Check Status
+      </button>
+      <p>{msgPlaceholder}</p>
+    </>
   );
 }
 
